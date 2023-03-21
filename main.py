40678f611d5ec7c2751aca21ba439712b5326701
Yohan Vinu <yohanvvinu@gmail.com>
2023-03-21 10:23:04 +0530

    Add streamlit GUI

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-import numpy as np
 import cv2
+import streamlit as st
+from datetime import datetime
 
-cap = cv2.VideoCapture(0)
+
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-# mouth_cascade = cv2.CascadeClassifier('haarcascade_mouth.xml')
 
-while True:
-    ret, frame = cap.read()
-    eyes_detected = False
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
-        roi_gray = gray[y:y+w, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
-        # mouth = mouth_cascade.detectMultiScale(roi_gray, 1.3, 5)
+st.title("Motion Detector")
+start = st.button("Start Camera")
 
-        # if len(mouth) > 0:
-        #     mouth_detected = True
-        #     for (mx, my, mw, mh) in mouth:
-        #         cv2.rectangle(roi_color, (mx, my), (mx + mw, my + mh), (0, 0, 255), 5)
 
-        if len(eyes) > 0:
-            eyes_detected = True
-            for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+if start:
+    streamlit_image = st.image([])
+    camera = cv2.VideoCapture(0)
+    while True:
+        check, frame = camera.read()
+        eyes_detected = False
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+            roi_gray = gray[y:y + w, x:x + w]
+            roi_color = frame[y:y + h, x:x + w]
+            eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
 
-    cv2.imshow('frame', frame)
-    print(eyes_detected)
-    if cv2.waitKey(1) == ord('q'):
-        break
+            if len(eyes) > 0:
+                eyes_detected = True
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh),
+                                  (0, 255, 0), 5)
 
-cap.release()
-cv2.destroyAllWindows()
+        print(eyes_detected)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+        streamlit_image.image(frame)
